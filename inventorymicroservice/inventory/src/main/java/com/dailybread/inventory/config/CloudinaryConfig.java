939fcd906f3c74f2324ae595a014ce50d90ed32d
Kinzang Dorji <kinzasdorji66@gmail.com>
2025-05-08 20:40:56 +0000

    cloudinary updates-removed dotenv

--- inventorymicroservice/inventory/src/main/java/com/dailybread/inventory/config/CloudinaryConfig.java
+++ inventorymicroservice/inventory/src/main/java/com/dailybread/inventory/config/CloudinaryConfig.java
@@ -1,19 +1,15 @@
 package com.dailybread.inventory.config;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
     @Bean
-    public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load(); // Make sure .env is at root
-        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+    public Cloudinary cloudinary(@Value("${CLOUDINARY_URL}") String cloudinaryUrl) {
+        return new Cloudinary(cloudinaryUrl);
     }
 }
